b2bb52ee5259f5d2e29ae824217c85fa137caee0
YuXiang Tong <60409201+team5-project@users.noreply.github.com>
2020-11-09 21:19:03 +0800

    定义初始变量

--- l1Filter.cs
+++ l1Filter.cs
@@ -6,5 +6,18 @@ namespace l1Filter
 {
     class l1Filter
     {
+        public static double Alpha = 0.01;
+        public static double Beta = 0.5;
+        public static int Mu = 2;
+        public static int Maxiter = 40;
+        public static int Maxlister = 20;
+        public static double Tol = 1e-4;
+
+        public static List<string> y = new List<string>();
+        public static int n = y.Count;
+        public static int m = n - 2;
+
+        public static Matrix I2 = Matrix.Speye(n - 2, n - 2);
+        public static Matrix O2 = Matrix.Zeros(n - 2, 1);
     }
 }
